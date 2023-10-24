93a930997915df6e9fbda2359a0105dfc6be4021
Samuel-olurotimi <samuel.olurotimi@decagon.dev>
2023-10-24 13:37:59 +0100

    first commit

---
+++ Task_1/Program.cs
@@ -0,0 +1,43 @@
+using Microsoft.EntityFrameworkCore;
+using Task_1.Db_Folder;
+using Task_1.Models;
+using Task_1.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+//builder.Services.AddDistributedRedisCache(options =>
+
+builder.Services.AddStackExchangeRedisCache(options =>
+{
+    options.Configuration = builder.Configuration.GetConnectionString("RedisConnection");
+    options.InstanceName = "RedisSample_";
+});
+
+
+builder.Services.AddDbContext<CustomerInfo_DbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("MyConnectionString")));
+
+builder.Services.AddScoped<IRedisCaching, RedisCaching>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
---
+++ Task_1/Services/RedisCaching.cs
@@ -0,0 +1,43 @@
+﻿
+using StackExchange.Redis;
+using System.Text.Json;
+
+namespace Task_1.Services
+{
+    public class RedisCaching : IRedisCaching
+    {
+       private IDatabase _cacheDb;
+        
+        public RedisCaching()
+        {
+            var redis = ConnectionMultiplexer.Connect("localhost:6379");   //For development enviroment
+
+            _cacheDb = redis.GetDatabase();
+        }
+        
+        public T GetData<T>(string key)
+        {
+           var value = _cacheDb.StringGet(key);
+            if(!string.IsNullOrEmpty(value))
+                return JsonSerializer.Deserialize<T> (value);
+
+            return default;
+        }
+
+        public object RemoveData(string key)
+        {
+           var _exist = _cacheDb.KeyExists(key);
+
+            if (_exist) 
+                return _cacheDb.KeyDelete(key);
+
+            return false;
+        }
+
+        public bool SetData<T>(string key, T vaue, DateTimeOffset expirationTime)
+        {
+            var expiryTime = expirationTime.DateTime.Subtract(DateTime.Now);
+            return _cacheDb.StringSet(key, JsonSerializer.Serialize(vaue), expiryTime);
+        }
+    }
+}
